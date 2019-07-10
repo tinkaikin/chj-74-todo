2ae229e0af760ff1cd26ff0a19ed08ac2ce617ce
tinkaikin <tinkaikin@163.com>
2019-07-10 13:33:31 +0800

    3.1.8 创建menus.vue(菜单组件)

--- src/components/layouts.vue
+++ src/components/layouts.vue
@@ -1,13 +1,19 @@
 <template>
   <section class="container" > <!--最外层容器-->
     <section class="menu"> <!--左边的容器-->
+      <menus></menus>
     </section>
     <section class="content-container"><!--右边的容器-->
     </section>
   </section>
 </template>
 <script>
-export default {}
+import menus from './menus'
+export default {
+  components: {
+    menus
+  }
+}
 </script>
 <style lang="less">
 @import '../a-common/style/layouts.less';
---
+++ src/components/menus.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="list-todos">  <!--菜单容器-->
+      <a class="list-todo activeListClass list" > <!--单个菜单容器-->
+        <span class="icon-lock" ></span>  <!--锁的图标-->
+        <span class="count-list">1</span><!--数字-->
+        星期一 <!--菜单内容-->
+      </a>
+    <a class=" link-list-new" > <!--新增菜单-->
+      <span class="icon-plus">
+      </span>
+      新增
+    </a>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style lang="less">
+@import '../a-common/style/menu.less';
+</style>
