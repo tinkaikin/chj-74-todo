f5f8a662800a057db58ff7fa34258440b41d3d28
tinkaikin <tinkaikin@163.com>
2019-07-10 13:43:36 +0800

    3.1.9列表渲染

--- src/components/menus.vue
+++ src/components/menus.vue
@@ -1,20 +1,35 @@
 <template>
-  <div class="list-todos">  <!--菜单容器-->
-      <a class="list-todo activeListClass list" > <!--单个菜单容器-->
-        <span class="icon-lock" ></span>  <!--锁的图标-->
-        <span class="count-list">1</span><!--数字-->
-        星期一 <!--菜单内容-->
-      </a>
-    <a class=" link-list-new" > <!--新增菜单-->
-      <span class="icon-plus">
-      </span>
+  <div class="list-todos">
+    <!--菜单容器-->
+    <a class="list-todo activeListClass list" v-for="(item, index) in items" :key="index">
+      <!--单个菜单容器-->
+      <span class="icon-lock" v-if="item.locked"></span>
+      <!--锁的图标-->
+      <span class="count-list" v-if="item.count >= 0">{{item.count}}</span>
+      <!--数字-->
+      {{item.title}}
+      <!--菜单内容-->
+    </a>
+    <a class="link-list-new">
+      <!--新增菜单-->
+      <span class="icon-plus"></span>
       新增
     </a>
   </div>
 </template>
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      items: [ // 菜单的模拟数据
+        { title: '星期一', count: 0, locked: true },
+        { title: '星期二', count: 2, locked: true },
+        { title: '星期三', count: 3, locked: false }
+      ]
+    }
+  }
+}
 </script>
 <style lang="less">
-@import '../a-common/style/menu.less';
+@import "../a-common/style/menu.less";
 </style>
